c92c809ab078b14b4ca7ce4028b4b3d4fdb0426b
Pedro Coutinho Rimes <pcoutinhorimes@gmail.com>
2021-10-01 23:51:27 -0300

    fim

--- projeto_DOM.js
+++ projeto_DOM.js
@@ -1,1 +1,11 @@
-
+// Envia mensagem
+let click = document.getElementById("Enviar");
+click.addEventListener( "click" , function (){
+    let msg = document.querySelector("#AreaDeTexto");
+    let msg2 = msg.value;
+    let nespaco = document.createElement("div");
+    let container = document.getElementById("histórico");
+    nespaco.innerText = msg2;
+    container.appendChild(nespaco);
+    nespaco.classList.add("spawnbox");
+})
